26a67507f2c9d2c6589b6f9f3102699b515b4ca9
Ancentkitonyi <ancentkitonyi.m@gmail.com>
2016-12-02 15:54:00 +0300

    Console app to print github repositories and languages used

---
+++ API-github_repo/github_repo.py
@@ -0,0 +1,14 @@
+import requests
+print "Console app to print github repositories and languages used"
+
+user = raw_input("Enter your GitHub Username: ")
+
+if user == None:
+	print "User not provided!"
+	
+response = requests.get('https://api.github.com/users/%s/repos' % user)
+
+assert response.status_code == 200
+
+for repo in response.json():
+    print repo['name'],"         --->      ", repo['language']
